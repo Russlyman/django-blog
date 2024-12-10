65faaa624d770468bb05201be2bf5c3a93ee1218
Ross Elliman <rosselliman@hotmail.com>
2024-12-10 13:50:47 +0000

    Give user feedback to indicate a comment is waiting for approval

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 from .forms import CommentForm
+from django.contrib import messages
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -35,6 +36,10 @@ def post_detail(request, slug):
             comment.author = request.user
             comment.post = post
             comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
 
     comment_form = CommentForm()
 
