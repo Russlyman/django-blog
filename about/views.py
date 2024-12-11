7076737a420a33bb9a3c149fb73bf38027dfa589
Ross Elliman <rosselliman@hotmail.com>
2024-12-11 14:49:18 +0000

    Save form data to database

--- about/views.py
+++ about/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import About
 from .forms import CollaborateRequestForm
+from django.contrib import messages
 
 # Create your views here.
 def about_view(request):
-    about = About.objects.all().order_by('-updated_on').first()
+    if request.method == "POST":
+        collaborate_form = CollaborateRequestForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
 
+    about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateRequestForm()
 
     return render(
